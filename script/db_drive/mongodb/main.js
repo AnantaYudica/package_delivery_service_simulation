6a4ba4ba851f8995e4e4ef302c6a2ba37e1a0219
Ananta Yudica <nta040689@gmail.com>
2019-10-24 07:05:08 +0700

    Updated field variable from using bool to int for flag process Added field variable for flag process connection Updated promise resolve not using parsing parameter Updated field method close to async function Updated field method close check process before close connection Added filed method insert for insert objects to mongodb

--- script/db_drive/mongodb/main.js
+++ script/db_drive/mongodb/main.js
@@ -12,36 +12,38 @@ module.exports = class Mongodb
         this.url = "";
         this.name = "";
         this.options = "";
-        this.is_process = false;
+        this.process_count = 0;
+        this.is_process_connect = false;
     }
     async connect(url, name, options)
     {
         if (this.is_open) close();
-        this.is_process = true;
+        this.is_process_connect = true;
         var callback = function (err, result)
         {
             new Promise(function(resolve, reject){
                 if (err) reject(err);
-                resolve(result, result.db(name));
+                resolve();
             }).catch(function(err) {
-                console.log(err);
-                this.is_process = false;
-            }.bind(this)).then(function(db, dbo){
-                if (db === undefined || dbo === undefined) return;
-                this.db = db;
-                this.dbo = dbo;
+                this.is_process_connect = false;
+                throw err;
+            }.bind(this)).then(function(){
+                this.db = result;
+                this.dbo = result.db(name);
                 this.url = url;
                 this.name = name;
                 this.options = options;
-                this.is_process = false;
                 this.is_connect = true;
+                this.is_process_connect = false;
             }.bind(this));
         }
         MongoClient.connect(url, options, callback.bind(this));
-        while(this.is_process) await sleep(100);
+        while(this.is_process_connect) await sleep(100);
     }
-    close()
+    async close()
     {
+        while(this.process_count !== 0 &&
+            !this.is_process_connect) await sleep(100);
         if (this.db !== undefined) this.db.close();
         this.is_connect = false;
         this.db = undefined;
@@ -49,6 +51,21 @@ module.exports = class Mongodb
         this.url = "";
         this.name = "";
         this.options = "";
-        this.is_process = false;
+        this.process_count = 0;
+        this.is_process_connect = false;
+    }
+    async insert(table, value, callback = function(err, res){
+        if (err) throw err;
+    })
+    {
+        if (!Array.isArray(value))
+            return this.insert(table, [value]);
+        if (!this.is_connect) throw new Error("No connection to database!");
+        this.process_count++;
+        var col = this.dbo.collection(table);
+        col.insertMany(value, function(err, res) {
+            callback(err, res);
+            this.process_count--;
+        }.bind(this));
     }
 };
